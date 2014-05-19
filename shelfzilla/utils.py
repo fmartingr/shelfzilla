a5b740b814a45a46904210a9eff2e395c57e5b18
Felipe Martin <fmartingr@me.com>
2014-05-20 00:19:19 +0200

    Re-fixed filename generator

--- shelfzilla/utils.py
+++ shelfzilla/utils.py
@@ -6,7 +6,7 @@ def filer_generate_randomized(instance, filename):
     uuid_str = u'{}'.format(uuid.uuid4())
     random_path = u"%s/%s/%s" % (uuid_str[0:2], uuid_str[2:4], uuid_str)
     name, extension = os.path.splitext(filename)
-    filename = u'{}.{}'.format(
+    filename = u'{}{}'.format(
         uuid.uuid4(), extension
     )
     return os.path.join(random_path, filename)
