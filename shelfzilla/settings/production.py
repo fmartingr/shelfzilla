1e3c0df55563b1c84ae9319900593fe7b6a724a5
Felipe Martin <fmartingr@me.com>
2014-05-26 17:27:03 +0200

    Updated production settings with MEDIA_URL and STATIC_URL settings. Tap #18

--- shelfzilla/settings/production.py
+++ shelfzilla/settings/production.py
@@ -15,7 +15,10 @@ INSTALLED_APPS = (
 )
 
 STATIC_ROOT = os.environ['APP_STATIC_ROOT']
+STATIC_URL = os.environ['APP_STATIC_URL']
+
 MEDIA_ROOT = os.environ['APP_MEDIA_ROOT']
+MEDIA_URL = os.environ['APP_MEDIA_URL']
 
 SECRET_KEY = os.environ['APP_SECRET_KEY']
 
