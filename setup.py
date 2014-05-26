a19c2f7ea021f5204fc5a075a9d06f602fc34da5
Felipe Martin <fmartingr@me.com>
2014-05-26 17:12:01 +0200

    Random tests are random

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='shelfzilla',
+    version='0.1.0',
+    author='Felipe Martin',
+    author_email='fmartingr@me.com',
+    packages=['shelfzilla']
+)
